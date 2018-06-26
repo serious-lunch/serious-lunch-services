c04ff069574f1ba16aac9b30a004b4e8744ea7d4
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-06-27 08:51:29 +0900

    add AccountCookies.scala

---
+++ sl-repository/src/main/scala/serious_lunch/repository/account_cookies/AccountCookies.scala
@@ -0,0 +1,15 @@
+package serious_lunch.repository.account_cookies
+
+import java.time.ZonedDateTime
+
+import serious_lunch.repository.core.SeriousLunchTable
+
+object AccountCookies
+  extends SeriousLunchTable[AccountCookies]("account_cookies")
+
+case class AccountCookies(
+    account_id: Long,
+    remember_digest: String,
+    created_at: ZonedDateTime,
+    updated_at: ZonedDateTime,
+)
