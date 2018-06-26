c7f4a4577079393da8f65f3fae5c24a0db348848
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-06-26 10:09:12 +0900

    add AccountActivations.scala

---
+++ sl-repository/src/main/scala/serious_lunch/repository/account_activations/AccountActivations.scala
@@ -0,0 +1,10 @@
+package serious_lunch.repository.net_pay
+
+import java.time.ZonedDateTime
+
+import serious_lunch.repository.core.SeriousLunchTable
+
+object AccountActivations extends SeriousLunchTable[AccountActivations]("account_activations")
+
+case class AccountActivations(
+)
