8779b920599b92d1bcc6635a29a2c711f48f8b37
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-06-28 00:09:34 +0900

    add AccountRelationsips.scala

---
+++ sl-repository/src/main/scala/serious_lunch/repository/account_relationships/AccountRelationsips.scala
@@ -0,0 +1,14 @@
+package serious_lunch.repository.account_relationships
+
+import java.time.ZonedDateTime
+
+import serious_lunch.repository.core.SeriousLunchTable
+
+object AccountRelationsips extends SeriousLunchTable[AccountRelationsips]("account_relationships")
+
+case class AccountRelationsips(
+    follower_account_id: Long,
+    followed_account_id: Long,
+    created_at: ZonedDateTime,
+    updated_at: ZonedDateTime,
+)
