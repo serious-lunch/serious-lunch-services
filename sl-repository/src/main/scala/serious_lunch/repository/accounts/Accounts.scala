cf6dcbd48f96a7faed443ac04b16a78e45c73b23
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-07-04 00:25:00 +0900

    fix Accounts.scala

--- sl-repository/src/main/scala/serious_lunch/repository/accounts/Accounts.scala
+++ sl-repository/src/main/scala/serious_lunch/repository/accounts/Accounts.scala
@@ -7,4 +7,10 @@ import serious_lunch.repository.core.SeriousLunchTable
 object Accounts extends SeriousLunchTable[Accounts]("accounts")
 
 case class Accounts(
+    account_id: Long,
+    account_name: String,
+    email_address: String,
+    password_digest: String,
+    created_at: ZonedDateTime,
+    updated_at: ZonedDateTime,
 )
