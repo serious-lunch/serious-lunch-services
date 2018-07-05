900db4258e49e6d58ac9ad70774eaccb4c6baedc
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-07-06 00:04:35 +0900

    fix Lunches.scala

--- sl-repository/src/main/scala/serious_lunch/repository/lunches/Lunches.scala
+++ sl-repository/src/main/scala/serious_lunch/repository/lunches/Lunches.scala
@@ -1,5 +1,6 @@
 package serious_lunch.repository.lunches
 
+import java.util.Date
 import java.time.ZonedDateTime
 
 import serious_lunch.repository.core.SeriousLunchTable
@@ -7,4 +8,10 @@ import serious_lunch.repository.core.SeriousLunchTable
 object Lunches extends SeriousLunchTable[Lunches]("lunches")
 
 case class Lunches(
+    lunch_id: Long,
+    account_id: Long,
+    lunch_date: Date,
+    comment: Option[String],
+    created_at: ZonedDateTime,
+    updated_at: ZonedDateTime,
 )
