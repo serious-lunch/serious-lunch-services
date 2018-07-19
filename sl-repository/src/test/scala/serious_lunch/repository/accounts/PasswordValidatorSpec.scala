655cfc3b852a2f3563eedbf167aed6c65d569d20
dyoshimitsu <d.yoshimitsu@gmail.com>
2018-07-19 22:35:13 +0900

    fix class name

--- sl-repository/src/test/scala/serious_lunch/repository/accounts/PasswordValidatorSpec.scala
+++ sl-repository/src/test/scala/serious_lunch/repository/accounts/PasswordValidatorSpec.scala
@@ -2,7 +2,7 @@ package serious_lunch.repository.accounts
 
 import org.scalatest.{FlatSpec, Matchers}
 
-class PasswordValidatorSpec extends FlatSpec with Matchers {
+class AccountAuthenticatorSpec extends FlatSpec with Matchers {
 
   it should "return true because of the correct password" in {
 
