fb31551240e264a4d168bfcbab3905e021779c24
dyoshimitsu <d.yoshimitsu@gmail.com>
2019-03-12 01:12:43 +0900

    add spec

---
+++ sl-play/test/controllers/v1/LunchesControllerSpec.scala
@@ -0,0 +1,16 @@
+package controllers.v1
+import org.scalatest.FunSuite
+
+class LunchesControllerSpec extends FunSuite {
+
+  test("testShow") {}
+
+  test("testUpdate") {}
+
+  test("testDestroy") {}
+
+  test("testIndex") {}
+
+  test("testCreate") {}
+
+}
