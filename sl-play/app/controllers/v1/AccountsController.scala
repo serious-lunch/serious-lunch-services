a62b40da8e3aa59c7431f6e48b75b90aa2a666a0
dyoshimitsu <d.yoshimitsu@gmail.com>
2019-03-14 22:11:09 +0900

    add AccountsController.show

--- sl-play/app/controllers/v1/AccountsController.scala
+++ sl-play/app/controllers/v1/AccountsController.scala
@@ -1,7 +1,9 @@
 package controllers.v1
 
 import javax.inject.{Inject, Singleton}
+import play.api.libs.json.Json
 import play.api.mvc.{AbstractController, AnyContent, ControllerComponents, Request}
+import serious_lunch.domain.account.AccountLoader
 
 @Singleton
 class AccountsController @Inject()(cc: ControllerComponents)
@@ -16,7 +18,12 @@ class AccountsController @Inject()(cc: ControllerComponents)
   }
 
   def show(account_id: Long) = Action { request: Request[AnyContent] =>
-    Ok
+    AccountLoader.findByAccountId(account_id) match {
+      case Right(account) =>
+        Ok(Json.toJson(account))
+      case Left(_) =>
+        BadRequest
+    }
   }
 
   def update(account_id: Long) = Action { request: Request[AnyContent] =>
