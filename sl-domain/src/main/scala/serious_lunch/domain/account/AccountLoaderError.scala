a234626bbe0fa680e5f14b7c5707724f1bd2e6c2
dyoshimitsu <d.yoshimitsu@gmail.com>
2019-03-17 20:18:12 +0900

    fix AccountLoaderError

--- sl-domain/src/main/scala/serious_lunch/domain/account/AccountLoaderError.scala
+++ sl-domain/src/main/scala/serious_lunch/domain/account/AccountLoaderError.scala
@@ -3,3 +3,5 @@ package serious_lunch.domain.account
 sealed trait AccountLoaderError
 
 case class UnexpectedError(cause: Throwable) extends AccountLoaderError
+
+case class AccountNotFound(message: String) extends AccountLoaderError
