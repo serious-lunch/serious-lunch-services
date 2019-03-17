4185a839da86466fa6c1ef08f57918e2456805f8
dyoshimitsu <d.yoshimitsu@gmail.com>
2019-03-17 20:20:03 +0900

    fix AccountLoader

--- sl-domain/src/main/scala/serious_lunch/domain/account/AccountLoader.scala
+++ sl-domain/src/main/scala/serious_lunch/domain/account/AccountLoader.scala
@@ -1,16 +1,32 @@
 package serious_lunch.domain.account
 
+import scalikejdbc.DBSession
 import serious_lunch.model.Account
+import serious_lunch.repository.DatabaseHolder
+import serious_lunch.repository.account.{AccountRecord, AccountStore}
 
 object AccountLoader {
 
   def findByAccountId(accountId: Long): Either[AccountLoaderError, Account] = {
     try {
-      Account(
-        accountId = accountId,
-      )
+      DatabaseHolder.connectWithSeriousLunch.localTx { implicit session =>
+        for {
+          account <- findAccount(accountId)
+        } yield {
+          Account(
+            accountId = account.account_id,
+          )
+        }
+      }
     } catch {
       case e: Exception => Left(UnexpectedError(e))
     }
+  }
+
+  private def findAccount(accountId: Long)(
+      implicit session: DBSession): Either[AccountNotFound, AccountRecord] = {
+    AccountStore
+      .findById(accountId)
+      .toRight(AccountNotFound(s"Not found account id : ${accountId}"))
   }
 }
