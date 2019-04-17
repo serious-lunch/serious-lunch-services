fdff3ba6f39d3901ab617809ec6f65c0df596404
dyoshimitsu <d.yoshimitsu@gmail.com>
2019-04-18 01:35:28 +0900

    fix SeriousLunchTable.scala

--- sl-repository/src/main/scala/serious_lunch/repository/core/SeriousLunchTable.scala
+++ sl-repository/src/main/scala/serious_lunch/repository/core/SeriousLunchTable.scala
@@ -3,6 +3,5 @@ package serious_lunch.repository.core
 import scalikejdbc.SQLSyntaxSupport
 
 class SeriousLunchTable[A](table: String) extends SQLSyntaxSupport[A] {
-  override val connectionPoolName = 'serious_lunch
   override val tableName = table
 }
