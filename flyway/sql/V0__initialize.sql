30b95cf9e9fc7013ee77771f0515c45cb5fa3319
dyoshimitsu <d.yoshimitsu@gmail.com>
2019-05-26 13:36:22 +0900

    fix initialize.sql

--- flyway/sql/V0__initialize.sql
+++ flyway/sql/V0__initialize.sql
@@ -70,7 +70,7 @@ INSERT INTO `accounts` (`account_id`, `account_name`, `email_address`, `password
 (1, 'foo', 'foo@example.com', '$2a$10$W/ddZ.N7db0O8xdljE70F.hhWcxainSDTGbDCV/bJuxv.ku.Mz6nq', '2019-05-26 03:40:57', '2019-05-26 03:40:57'),
 (2, 'bar', 'bar@example.com', '$2a$10$gQKve8lQqggP6yxqCOV8MuTy22VO01egg6lyOGFTpXTsOz2Q1dfYq', '2019-05-26 03:40:57', '2019-05-26 03:40:57');
 
-INSERT INTO `account_activations` (    `account_id`,    `activation_digest`, `activated`, `created_at`, `updated_at`) VALUES
+INSERT INTO `account_activations` (`account_id`, `activation_digest`, `activated`, `created_at`, `updated_at`) VALUES
 (1, '', TRUE, '2019-05-26 03:40:57', '2019-05-26 03:40:57'),
 (2, '', TRUE, '2019-05-26 03:40:57', '2019-05-26 03:40:57');
 
