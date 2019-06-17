97b909621d4a57f169049129edcc5d4a33bd99dc
dyoshimitsu <d.yoshimitsu@gmail.com>
2019-06-18 01:52:15 +0900

    fix create table

--- docker/flyway/development/sql/V0_00__initialize.sql
+++ docker/flyway/development/sql/V0_00__initialize.sql
@@ -1,67 +1,87 @@
-SET NAMES utf8mb4 ;
-SET character_set_client = utf8mb4 ;
+create table accounts
+(
+    account_id      bigint auto_increment
+        primary key,
+    account_name    varchar(50)  not null,
+    email_address   varchar(255) not null,
+    password_digest varchar(255) not null,
+    created_at      datetime     not null,
+    updated_at      datetime     not null,
+    constraint index_accounts_on_account_name
+        unique (account_name),
+    constraint index_accounts_on_email_address
+        unique (email_address)
+);
 
-CREATE TABLE `accounts` (
-                            `account_id` bigint(20) NOT NULL AUTO_INCREMENT,
-                            `account_name` varchar(50) COLLATE utf8mb4_bin NOT NULL,
-                            `email_address` varchar(255) COLLATE utf8mb4_bin NOT NULL,
-                            `password_digest` varchar(255) COLLATE utf8mb4_bin NOT NULL,
-                            `created_at` datetime NOT NULL,
-                            `updated_at` datetime NOT NULL,
-                            PRIMARY KEY (`account_id`),
-                            UNIQUE KEY `index_accounts_on_account_name` (`account_name`),
-                            UNIQUE KEY `index_accounts_on_email_address` (`email_address`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+create table account_activations
+(
+    account_id        bigint               not null,
+    activation_digest varchar(255)         not null,
+    activated         tinyint(1) default 0 not null,
+    created_at        datetime             not null,
+    updated_at        datetime             not null,
+    constraint index_account_activations_on_account_id
+        unique (account_id),
+    constraint fk_rails_d7d52ac162
+        foreign key (account_id) references accounts (account_id)
+);
 
-CREATE TABLE `account_activations` (
-                                       `account_id` bigint(20) NOT NULL,
-                                       `activation_digest` varchar(255) COLLATE utf8mb4_bin NOT NULL,
-                                       `activated` tinyint(1) NOT NULL DEFAULT '0',
-                                       `created_at` datetime NOT NULL,
-                                       `updated_at` datetime NOT NULL,
-                                       UNIQUE KEY `index_account_activations_on_account_id` (`account_id`),
-                                       KEY `index_account_activations_on_activated` (`activated`),
-                                       CONSTRAINT `fk_rails_d7d52ac162` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`account_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+create index index_account_activations_on_activated
+    on account_activations (activated);
 
-CREATE TABLE `account_cookies` (
-                                   `account_id` bigint(20) NOT NULL,
-                                   `remember_digest` varchar(255) COLLATE utf8mb4_bin NOT NULL,
-                                   `created_at` datetime NOT NULL,
-                                   `updated_at` datetime NOT NULL,
-                                   UNIQUE KEY `index_account_cookies_on_account_id` (`account_id`),
-                                   CONSTRAINT `fk_rails_a09f491abe` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`account_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+create table account_cookies
+(
+    account_id      bigint       not null,
+    remember_digest varchar(255) not null,
+    created_at      datetime     not null,
+    updated_at      datetime     not null,
+    constraint index_account_cookies_on_account_id
+        unique (account_id),
+    constraint fk_rails_a09f491abe
+        foreign key (account_id) references accounts (account_id)
+);
 
-CREATE TABLE `account_relationships` (
-                                         `account_relationship_id` bigint(20) NOT NULL AUTO_INCREMENT,
-                                         `follower_account_id` bigint(20) NOT NULL,
-                                         `followed_account_id` bigint(20) NOT NULL,
-                                         `created_at` datetime NOT NULL,
-                                         `updated_at` datetime NOT NULL,
-                                         PRIMARY KEY (`account_relationship_id`),
-                                         UNIQUE KEY `index_account_relationships_on_follower_and_followed` (`follower_account_id`,`followed_account_id`),
-                                         UNIQUE KEY `index_account_relationships_on_followed_and_follower` (`followed_account_id`,`follower_account_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+create table account_relationships
+(
+    account_relationship_id bigint auto_increment
+        primary key,
+    follower_account_id     bigint   not null,
+    followed_account_id     bigint   not null,
+    created_at              datetime not null,
+    updated_at              datetime not null,
+    constraint index_account_relationships_on_followed_and_follower
+        unique (followed_account_id, follower_account_id),
+    constraint index_account_relationships_on_follower_and_followed
+        unique (follower_account_id, followed_account_id)
+);
 
-CREATE TABLE `account_resets` (
-                                  `account_id` bigint(20) NOT NULL,
-                                  `reset_digest` varchar(255) COLLATE utf8mb4_bin NOT NULL,
-                                  `created_at` datetime NOT NULL,
-                                  `updated_at` datetime NOT NULL,
-                                  UNIQUE KEY `index_account_resets_on_account_id` (`account_id`),
-                                  CONSTRAINT `fk_rails_7312fec1e5` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`account_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+create table account_resets
+(
+    account_id   bigint       not null,
+    reset_digest varchar(255) not null,
+    created_at   datetime     not null,
+    updated_at   datetime     not null,
+    constraint index_account_resets_on_account_id
+        unique (account_id),
+    constraint fk_rails_7312fec1e5
+        foreign key (account_id) references accounts (account_id)
+);
 
-CREATE TABLE `lunches` (
-                           `lunch_id` bigint(20) NOT NULL AUTO_INCREMENT,
-                           `account_id` bigint(20) NOT NULL,
-                           `lunch_date` date NOT NULL,
-                           `comment` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
-                           `created_at` datetime NOT NULL,
-                           `updated_at` datetime NOT NULL,
-                           PRIMARY KEY (`lunch_id`),
-                           KEY `index_lunches_on_account_id_and_lunch_date` (`account_id`,`lunch_date`),
-                           KEY `index_lunches_on_account_id_and_created_at` (`account_id`,`created_at`),
-                           CONSTRAINT `fk_rails_38eef2a231` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`account_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+create table lunches
+(
+    lunch_id   bigint auto_increment
+        primary key,
+    account_id bigint       not null,
+    lunch_date date         not null,
+    comment    varchar(255) null,
+    created_at datetime     not null,
+    updated_at datetime     not null,
+    constraint fk_rails_38eef2a231
+        foreign key (account_id) references accounts (account_id)
+);
+
+create index index_lunches_on_account_id_and_created_at
+    on lunches (account_id, created_at);
+
+create index index_lunches_on_account_id_and_lunch_date
+    on lunches (account_id, lunch_date);
