18c80f2e3ad8b4f995cb0afcbdd723ba66a109c9
dyoshimitsu <d.yoshimitsu@gmail.com>
2019-06-17 21:03:44 +0900

    refactor insert data

--- docker/flyway/development/sql/V0_00__initialize.sql
+++ docker/flyway/development/sql/V0_00__initialize.sql
@@ -65,19 +65,3 @@ CREATE TABLE `lunches` (
                            KEY `index_lunches_on_account_id_and_created_at` (`account_id`,`created_at`),
                            CONSTRAINT `fk_rails_38eef2a231` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`account_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
-INSERT INTO `accounts` (`account_id`, `account_name`, `email_address`, `password_digest`, `created_at`, `updated_at`) VALUES
-(1, 'foo', 'foo@example.com', '$2a$10$W/ddZ.N7db0O8xdljE70F.hhWcxainSDTGbDCV/bJuxv.ku.Mz6nq', '2019-05-26 03:40:57', '2019-05-26 03:40:57'),
-(2, 'bar', 'bar@example.com', '$2a$10$gQKve8lQqggP6yxqCOV8MuTy22VO01egg6lyOGFTpXTsOz2Q1dfYq', '2019-05-26 03:40:57', '2019-05-26 03:40:57');
-
-INSERT INTO `account_activations` (`account_id`, `activation_digest`, `activated`, `created_at`, `updated_at`) VALUES
-(1, '', TRUE, '2019-05-26 03:40:57', '2019-05-26 03:40:57'),
-(2, '', TRUE, '2019-05-26 03:40:57', '2019-05-26 03:40:57');
-
-INSERT INTO `account_relationships` (`follower_account_id`, `followed_account_id`, `created_at`, `updated_at`) VALUES
-(1, 2, '2019-05-26 03:40:57', '2019-05-26 03:40:57');
-
-INSERT INTO `lunches` (`lunch_id`, `account_id`, `lunch_date`, `comment`, `created_at`, `updated_at`) VALUES
-(1, 1, '2018-03-09', 'excellent', '2019-05-26 03:40:57', '2019-05-26 03:40:57'),
-(2, 2, '2018-03-09', 'bad', '2019-05-26 03:40:57', '2019-05-26 03:40:57'),
-(3, 1, '2018-03-08', 'good', '2019-05-26 03:40:57', '2019-05-26 03:40:57');
---
+++ docker/flyway/development/sql/V0_01__insert_development_data.sql
@@ -0,0 +1,15 @@
+INSERT INTO `accounts` (`account_id`, `account_name`, `email_address`, `password_digest`, `created_at`, `updated_at`) VALUES
+(1, 'foo', 'foo@example.com', '$2a$10$W/ddZ.N7db0O8xdljE70F.hhWcxainSDTGbDCV/bJuxv.ku.Mz6nq', '2019-05-26 03:40:57', '2019-05-26 03:40:57'),
+(2, 'bar', 'bar@example.com', '$2a$10$gQKve8lQqggP6yxqCOV8MuTy22VO01egg6lyOGFTpXTsOz2Q1dfYq', '2019-05-26 03:40:57', '2019-05-26 03:40:57');
+
+INSERT INTO `account_activations` (`account_id`, `activation_digest`, `activated`, `created_at`, `updated_at`) VALUES
+(1, '', TRUE, '2019-05-26 03:40:57', '2019-05-26 03:40:57'),
+(2, '', TRUE, '2019-05-26 03:40:57', '2019-05-26 03:40:57');
+
+INSERT INTO `account_relationships` (`follower_account_id`, `followed_account_id`, `created_at`, `updated_at`) VALUES
+(1, 2, '2019-05-26 03:40:57', '2019-05-26 03:40:57');
+
+INSERT INTO `lunches` (`lunch_id`, `account_id`, `lunch_date`, `comment`, `created_at`, `updated_at`) VALUES
+(1, 1, '2018-03-09', 'excellent', '2019-05-26 03:40:57', '2019-05-26 03:40:57'),
+(2, 2, '2018-03-09', 'bad', '2019-05-26 03:40:57', '2019-05-26 03:40:57'),
+(3, 1, '2018-03-08', 'good', '2019-05-26 03:40:57', '2019-05-26 03:40:57');
